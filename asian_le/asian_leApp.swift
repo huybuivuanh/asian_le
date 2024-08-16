021cc45a42cb69f2b8880a4f054103396564815a
Bùi Vũ Anh Huy <buivuanhhuy@Buis-MacBook-Pro.local>
2024-08-15 19:13:42 -0600

    Initial Commit

---
+++ asian_le/asian_leApp.swift
@@ -0,0 +1,17 @@
+//
+//  asian_leApp.swift
+//  asian_le
+//
+//  Created by Bùi Vũ Anh Huy on 2024-08-15.
+//
+
+import SwiftUI
+
+@main
+struct asian_leApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
